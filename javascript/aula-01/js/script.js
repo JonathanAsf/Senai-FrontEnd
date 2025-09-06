aa4090097e410dce76ee32cd89d2511ed37e5c86
Jonathan <jonathan.a.farias@gmail.com>
2025-09-06 13:46:20 -0300

    feat: basic syntax exercise in javascript

---
+++ javascript/aula-01/js/script.js
@@ -0,0 +1,8 @@
+const anoNascimento = 2003
+let anoAtual = 2025
+const nome = "Jonathan Almeida"
+const pi = 3.14
+
+console.log(pi)
+
+console.log (`Meu nome é ${nome}, tenho ${anoAtual-anoNascimento} anos de idade!`)
