2f827e6e1e55fab6592c7c57c980b241c1c1bce5
Jonathan <jonathan.a.farias@gmail.com>
2025-09-06 14:12:53 -0300

    feat: arrays examples added

---
+++ javascript/aula-01/js/arrays.js
@@ -0,0 +1,19 @@
+let cores = ["azul","vermelho","amarelo","branco"]
+let professores = ["Fontes", "Lucas", "Matheus"]
+let treinoSuperioresA = ["Peito","Triceps"]
+let treinoSuperioresB = ["Biceps","Dorsal"]
+let treinoInferiores = ["Quadriceps","Posterior","Panturrilha"]
+let cardios = ["esteira","escada","bicicleta indoor"]
+let frutas = ["bananas","maçã","uva","melancia"]
+let numerosLoteria = [150,15,300]
+
+console.log(`As cores permitidas são: ${cores}`)
+console.log(`Meus professores do senai foram: ${professores}`)
+console.log(`Meu treino favorito é ${treinoSuperioresA}`)
+console.log(`Eu prefiro treinar: ${treinoSuperioresB[0]}`)
+console.log(cardios + " são tipos de cardios")
+
+console.log(frutas[0],frutas[1])
+
+console.log(`A soma dos primeiros números da loteria é: ${numerosLoteria[0]} + ${numerosLoteria[1]}`)
+console.log(`A soma dos primeiros números da loteria é: ${numerosLoteria[0] + numerosLoteria[1]}`)
