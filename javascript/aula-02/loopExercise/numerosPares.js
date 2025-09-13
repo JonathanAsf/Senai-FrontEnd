d07e6c2b0ea827aa6cbe16a27e8724e235e8dbde
Jonathan <jonathan.a.farias@gmail.com>
2025-09-13 14:27:23 -0300

    feat: number 5 multiplication

--- javascript/aula-02/loopExercise/numerosPares.js
+++ javascript/aula-02/loopExercise/numerosPares.js
@@ -1,4 +1,7 @@
 // Use o for para mostrar apenas os números pares de 0 até 20.
-for(i=0;i<=20;i=i+2){
-    console.log(i)
+for(i=0;i<=20;i){
+    if (i % 2 == 0){
+        console.log(i)
+    }
+    i++
 }
---
+++ javascript/aula-02/loopExercise/tabuada5.js
@@ -0,0 +1,4 @@
+// Crie um for que mostre a tabuada do 5, do 5 x 1 até o 5 x 10.
+for(i=0;i<=10;i++){
+    console.log(`5x${i}: ${5*i}`)
+}
