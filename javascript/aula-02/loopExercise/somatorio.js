03286b4cd99d050c1be17192b4da77db12200dc2
Jonathan <jonathan.a.farias@gmail.com>
2025-09-13 15:57:08 -0300

    feat: sum exercise

---
+++ javascript/aula-02/loopExercise/somatorio.js
@@ -0,0 +1,10 @@
+// Some todos os números de 1 a 100 usando for.
+
+// No final, mostre o total no console.
+
+// Obs: Resultado deve ser 5050
+
+for(i=1;i<=100;i++){
+    numero = ((i*(i+1))/2) 
+}
+console.log(numero)
