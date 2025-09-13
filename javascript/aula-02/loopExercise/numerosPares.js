6ac80862e883ed33af7f9adc207814e05aa2f66b
Jonathan <jonathan.a.farias@gmail.com>
2025-09-13 14:46:54 -0300

    feat: pair numbers

--- javascript/aula-02/loopExercise/numerosPares.js
+++ javascript/aula-02/loopExercise/numerosPares.js
@@ -1,7 +1,6 @@
 // Use o for para mostrar apenas os números pares de 0 até 20.
-for(i=0;i<=20;i){
-    if (i % 2 == 0){
-        console.log(i)
-    }
-    i++
-}
+for(i=0;i<=20;i++){
+     if (i % 2 == 0){
+         console.log(i)
+     }
+ }
