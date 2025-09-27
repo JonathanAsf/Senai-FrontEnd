98664168850107af5e15fb8a9862de2cb5ee97d1
Jonathan <jonathan.a.farias@gmail.com>
2025-09-27 12:02:45 -0300

    feat(javascript):added function

---
+++ javascript/aula-03/dobro/script.js
@@ -0,0 +1,4 @@
+// Crie uma função chamada `somar` que receba dois números como parâmetro e exiba a soma no console.
+numero1 = Number(prompt("Digite o primeiro numero"))
+
+alert(` dos dois números é:${numero1*2}`)
---
+++ javascript/aula-03/soma/script.js
@@ -0,0 +1,5 @@
+// Crie uma função chamada `somar` que receba dois números como parâmetro e exiba a soma no console.
+numero1 = Number(prompt("Digite o primeiro numero"))
+numero2 = Number(prompt("Digite o segundo numero"))
+
+alert(`A soma dos dois números é:${numero1+numero2}`)
