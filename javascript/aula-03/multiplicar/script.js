db554a45dc2f57ced1daf69751be09cf1367a80f
jonathan <jonathan.a.farias@gmail.com>
2025-09-30 11:08:15 -0300

    feat(javascript): Exercise multiply

---
+++ javascript/aula-03/multiplicar/script.js
@@ -0,0 +1,10 @@
+// Crie uma função chamada multiplicar que receba dois número como parâmetro e exiba o resultado dessa multiplicação no console.
+
+numero1 = Number(prompt("Digite o primeiro numero: "))
+numero2 = Number(prompt("Digite o segundo numero: "))
+
+function soma (numero1,numero2){
+    return (numero1*numero2)
+}
+
+alert(`A soma dos dois números é: ${soma(numero1, numero2)}`)
