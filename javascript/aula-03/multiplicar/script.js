be040ecffdd4deb6f4e3076973be2b98be2490f4
jonathan <jonathan.a.farias@gmail.com>
2025-09-30 11:13:15 -0300

    feat(javascript): Exercise triple

--- javascript/aula-03/multiplicar/script.js
+++ javascript/aula-03/multiplicar/script.js
@@ -3,8 +3,8 @@
 numero1 = Number(prompt("Digite o primeiro numero: "))
 numero2 = Number(prompt("Digite o segundo numero: "))
 
-function soma (numero1,numero2){
+function multiplica (numero1,numero2){
     return (numero1*numero2)
 }
 
-alert(`A soma dos dois números é: ${soma(numero1, numero2)}`)
+alert(`A multiplicação dos dois números é: ${multiplica(numero1, numero2)}`)
---
+++ javascript/aula-03/triplo/script.js
@@ -0,0 +1,8 @@
+// Crie uma função chamada triplo(numero) que receba um número e retorne o triplo dele.
+numero = Number(prompt("Digite um número: "))
+
+function triplo (numero){
+    return (numero*3)
+}
+
+alert(`O triplo desse número é: ${triplo(numero)}`)
