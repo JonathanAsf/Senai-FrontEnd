61a37f5185fa0b5d62eb2bf83143b2d0d760e208
Jonathan <jonathan.a.farias@gmail.com>
2025-10-04 08:42:58 -0300

    feat(javascript): consuming api examples:

---
+++ javascript/aula-04/api/api.js
@@ -0,0 +1,14 @@
+// fetch("https://jsonplaceholder.typicode.com/users")
+//      .then ((res) => res.json())
+//      .then((res) => console.log(res[7].name))
+
+let nome = document.getElementById("nome")
+let username = document.getElementById("username")
+
+fetch("https://jsonplaceholder.typicode.com/users")
+    .then((res) => res.json())
+    .then((data) =>{
+        console.log(data[0])
+            nome.innerText = data[0].name
+            username.innerText = data[0].username
+    } )
