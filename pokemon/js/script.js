a9bfdceb4a740a7d32c9b0215636768444b83f40
Jonathan <jonathan.a.farias@gmail.com>
2025-10-11 11:27:19 -0300

    feat: previous and next button

--- pokemon/js/script.js
+++ pokemon/js/script.js
@@ -4,6 +4,8 @@ let pokemon_number = document.getElementById("pokemon_number")
 let pokemon_name = document.getElementById("pokemon_name")
 
 let btn_search = document.getElementById("btn-search")
+let btn_next = document.getElementById("btn-next")
+let btn_prev = document.getElementById("btn-prev")
 
 let search_pokemon = 1
 
@@ -16,10 +18,23 @@ function renderPokemon(pokemon){
         pokemon_number.innerText = data.id
         pokemon_image.src = data.sprites.versions["generation-v"]["black-white"].animated.front_default
         input_search.value = ""
+        search_pokemon = data.id
     })
 }
 
 btn_search.addEventListener("click",(e) => {
     e.preventDefault()
     renderPokemon(input_search.value)
+})
+
+btn_next.addEventListener("click", () => {
+    search_pokemon ++
+    renderPokemon(search_pokemon)
+})
+
+btn_prev.addEventListener("click", () => {
+    if(search_pokemon > 1){
+    search_pokemon --
+    renderPokemon(search_pokemon)    
+    }
 })
